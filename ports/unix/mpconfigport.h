5bdc89f373731b554f51cda8d172e5a920eda028
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-09-11 13:53:10 +0300

    unix: Add a way to "freeze" testsuite into the executable and run it such
    This is a generalization of functionality previously available in qemu-arm
    and zephyr ports. It's proposed to call such a feature "frozen testsuite",
    following existing "frozen modules" and "frozen bytecode".
    
    The motivation is similar to the above mentioned ports: while unix port
    definitely has access to filesystem, providing access to hundreds of
    test files may be cumersome or impossible (e.f. filesystem size limitation)
    for small, embedded platforms.
    
    Implementing print() output capture for unix port required some hacking
    on the level of py/streams.c.

--- ports/unix/mpconfigport.h
+++ ports/unix/mpconfigport.h
@@ -342,4 +342,14 @@ void mp_unix_mark_exec(void);
 #include <stdio.h>
 #endif
 
+#ifdef MP_FROZEN_TESTSUITE
+#include "lib/upytesthelper/upytesthelper.h"
+#undef MP_PLAT_PRINT_STRN
+#define MP_PLAT_PRINT_STRN(str, len) upytest_output(str, len)
+#undef MICROPY_ERROR_PRINTER
+#define MICROPY_ERROR_PRINTER (&mp_plat_print)
+// Somewhat hacky way to deinitialize mmap_region_head
+#define MICROPY_PORT_DEINIT_FUNC MP_STATE_VM(mmap_region_head) = NULL
+#endif
+
 #endif // MICROPY_UNIX_MINIMAL
