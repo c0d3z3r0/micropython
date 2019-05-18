8c08430f963d8f7db4a2227b750a93dc2305b0ca
Michael Niewöhner <foss@mniewoehner.de>
2019-05-18 21:02:15 +0200

    Add gpio mapping module for esp8266

---
+++ ports/esp8266/modules/gpio.py
@@ -0,0 +1,20 @@
+A0     =  0
+GPIO0  =  0
+GPIO2  =  2
+GPIO4  =  4
+GPIO5  =  5
+GPIO12 = 12
+GPIO13 = 13
+GPIO14 = 14
+GPIO15 = 15
+GPIO16 = 16
+D0     = GPIO16
+D1     = GPIO5
+D2     = GPIO4
+D3     = GPIO0
+D4     = GPIO2
+D5     = GPIO14
+D6     = GPIO12
+D7     = GPIO13
+D8     = GPIO15
+
