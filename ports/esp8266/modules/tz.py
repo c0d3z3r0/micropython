9c08522d6f68952ed94be3f282606885e2d918f7
Michael Niewöhner <foss@mniewoehner.de>
2019-05-28 19:48:49 +0200

    unix+esp8266: Rework tz modules

--- ports/esp8266/modules/tz.py
+++ ports/esp8266/modules/tz.py
@@ -1,26 +1,39 @@
 import utime
 
+TZ = 1  # UTC+1
+
 def lastSundayOfMonth(year, month):
     last_day_ts = utime.mktime((year, month + 1, 0, 0, 0, 0, 0, 0, -1))
     last_weekday = utime.localtime(last_day_ts)[6] + 1
     return last_day_ts - (last_weekday * 86400)
 
-cur_year = utime.localtime()[0]
-# dst start/end is at 01:00 UTC
-DSTSTART = lastSundayOfMonth(cur_year, 3) + 3600
-DSTEND   = lastSundayOfMonth(cur_year, 10) + 3600
-TZ = 1  # UTC+1
+def isdst(ts):
+    year = utime.localtime(ts)[0]
+    dst_start = lastSundayOfMonth(year, 3) + 3600
+    dst_end = lastSundayOfMonth(year, 10) + 3600
+    return (dst_start < ts < dst_end)
 
-def time():
-    t = utime.time() + TZ
-    if DSTSTART < t < DSTEND:
-        t += 1
-    return int(t)
+def utcoffset(ts=None):
+    if ts is None:
+        ts = utime.time()
+    offset = TZ * 3600
+    if isdst(ts):
+        offset += 3600
+    return offset
 
 def localtime(ts=None):
     if ts is None:
-        ts = time()
-    t = utime.localtime(ts)
-    if DSTSTART < ts < DSTEND:
-        t = t[:-1] + (1,)
+        ts = utime.time()
+    dst = isdst(ts)
+    ts += TZ * 3600
+    if dst:
+        ts += 3600
+    t = utime.localtime(ts)[:8] + (int(dst),)
     return t
+
+def mktime(lt):
+    ts = utime.mktime(lt)
+    ts -= TZ * 3600
+    if lt[8] == 1:
+        ts -= 3600
+    return ts
--- ports/unix/modules/tz.py
+++ ports/unix/modules/tz.py
@@ -1,2 +1,18 @@
-# dummy tz module
-from utime import time, localtime
+# dummy/proxy tz module
+
+import utime
+
+from utime import mktime, localtime
+
+TZ = 1  # UTC+1
+
+def isdst(ts):
+    return bool(utime.localtime(ts)[8])
+
+def utcoffset(ts=None):
+    if ts is None:
+        ts = utime.time()
+    offset = TZ * 3600
+    if isdst(ts):
+        offset += 3600
+    return offset
